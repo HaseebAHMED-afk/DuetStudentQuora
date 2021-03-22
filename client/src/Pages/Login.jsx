5a99b2bfa4b3454e679f2cbb03b8274881370ba6
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-03-22 15:18:04 +0500

    Login page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import '../App.css'
+import { Button } from '@material-ui/core'
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='login-page' >
+            <h1 className='login-heading' >You needto login to continue</h1>
+            <div className="login-form">
+                <TextField variant='outlined' color='primary' label='Email' className='login-input' />
+                <TextField variant='outlined' color='primary' label='Password' className='login-input' />
+                <Button variant='contained' color='primary' >Login</Button>
+            </div>
         </div>
     )
 }

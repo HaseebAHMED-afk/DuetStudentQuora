bc798b11e28c37bef2486a15664907440a6c77f0
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-03-28 22:46:34 +0500

    On server

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import '../App.css'
 import { Button } from '@material-ui/core'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
@@ -24,10 +25,11 @@ const Login = () => {
     }
     return (
         <div className='login-page' >
-            <h1 className='login-heading' >You needto login to continue</h1>
+            <h1 className='login-heading' >You need to login to continue</h1>
             <div className="login-form">
                 <TextField variant='outlined' color='primary' label='Email' className='login-input' type='Email' value={email} onChange={(e)=>{setEmail(e.target.value)}} />
                 <TextField variant='outlined' color='primary' label='Password' type='password' className='login-input' value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                <p style={{fontFamily:'monospace', margin:15}} >Not a member? <span> <Link to='/signup'  >Sign Up</Link> </span></p>
                 <Button variant='contained' onClick={handleSubmit} color='primary' >Login</Button>
             </div>
         </div>
---
+++ server/app.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/duetDB', {useCreateIndex: true , useNewUrlParser: true, useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
+
+const newUserSchema = new Schema({
+    userEmail: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userPassword: {
+        type: String,
+        required: true,
+    }
+})
+
+const newUser = mongoose.model('NewUser',newUserSchema);
+
+const newProfileSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    fatherName: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    cnic:{
+        type: Number,
+        required: true
+    },
+    bloodGroup:{
+        type: String,
+        required: true
+    },
+    rollNo: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    batch:{
+        type: String,
+        required: true
+    },
+    enrolement:{
+        type: String,
+        required: true
+    },
+    contact:{
+        type: String,
+        required: true
+    }
+})
+
+const newProfile = mongoose.model("newProfile", newProfileSchema)
+
+const connection = mongoose.connection;
+
+connection.once('open', ()=> {
+    console.log('DB connection established');
+});
+
+
+app.post('/signup' , (req,res)=> {
+
+
+   const userEmail = req.body.email;
+   const userPassword = req.body.password;
+
+
+   const User = newUser({
+       userEmail,
+       userPassword
+   })
+
+   User.save().then(()=>{res.json('User added')})
+   .catch((err)=> res.status(400).json('Error:'+err))
+
+
+})
+
+app.post('/buildprofile' , (req,res) => {
+    const profile = new newProfile({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        fatherName: req.body.fatherName,
+        gender: req.body.gender,
+        cnic: req.body.cnic,
+        bloodGroup: req.body.bloodGroup,
+        rollNo: req.body.rollNo,
+        designation: req.body.designation,
+        department: req.body.department,
+        batch: req.body.batch,
+        enrolement: req.body.enrolement,
+        contact: req.body.contact
+    })
+
+    profile.save().then(()=>{res.json('Profile added')})
+    .catch((err)=>res.status(400).json('Error:'+err))
+})
+
+
+app.listen(5000 , () => {
+    console.log('Server running on port 5000');
+});
